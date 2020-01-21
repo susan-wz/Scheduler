23a4f1174222dadcf94022fadc99b7ec45b9d6d2
Susan Zhang <susanwenzhang@gmail.com>
2020-01-20 20:10:49 -0500

    prior to working on appointment form

---
+++ src/components/DayListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from 'classnames/bind';
+
+export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots===0
+  })
+
+  const formatSpots = function(daysRemaining) {
+    if (daysRemaining === 0) {
+      return "no spots remaining"
+    } else if (daysRemaining === 1) {
+      return daysRemaining += " spot remaining"
+    } else {
+      return daysRemaining += " spots remaining"
+    }
+  }
+
+  return (
+    <li 
+    className={dayClass}
+    onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
