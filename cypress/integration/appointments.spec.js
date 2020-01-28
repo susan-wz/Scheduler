9df0b2767c8df0df7cc25cf49c779256d29d14f3
Susan Zhang <susanwenzhang@gmail.com>
2020-01-28 14:04:20 -0500

    final edit to testing

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -6,7 +6,7 @@ describe("Appointments", () => {
     cy.contains('[data-testid=day]', "Monday")
   })
 
-  xit("should book an interview", () => {
+  it("should book an interview", () => {
   
     cy.get('[alt=Add]').first().click()
 
@@ -20,7 +20,7 @@ describe("Appointments", () => {
 
   });
 
-  xit("should edit an interview", () => {
+  it("should edit an interview", () => {
     cy.get("[alt=Edit]").first().click({ force: true })
     cy.get("[alt='Tori Malcolm']").click()
     cy.get("[data-testid=student-name-input]").clear().type("Test Name")
@@ -32,7 +32,7 @@ describe("Appointments", () => {
   });
 
   it("should cancel an interview", () => {
-    cy.get("[alt=Delete]").first().click({ force: true })
+    cy.get("[alt=Delete]").click({ force: true })
     cy.contains("Confirm").click();
 
     cy.contains("Deleting")
