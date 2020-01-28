089cf8a5e233cfee6b8275b280c1084ff1cfb7d1
Susan Zhang <susanwenzhang@gmail.com>
2020-01-27 20:25:27 -0500

    completed adding tests for application data functions

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,31 +1,6 @@
 import axios from "axios";
 import { useEffect, useReducer } from "react";
-
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return { ...state, day: action.day }
-    case SET_APPLICATION_DATA:
-      return { ...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers }
-    case SET_INTERVIEW:
-      const { id, interview } = action;
-      return {
-        ...state,
-        appointments: {
-          ...state.appointments,
-          [id]: {
-            ...state.appointments[action.id],
-            interview: action.interview ? { ...interview } : null
-          }
-        }
-      }
-    default: throw new Error(`Tried to reduce with unsupported action type: ${action.type}`)
-  }
-}
+import reducer, { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW } from "reducers/application"
 
 export default function useApplicationData() {
   const [state, dispatch] = useReducer(reducer, {
